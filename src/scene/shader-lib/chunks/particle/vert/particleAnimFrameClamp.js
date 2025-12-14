e1c6c734d0a8b7adc9c5aa21a2f973bb4a32bfc1
aiab <aidinabedi@gmail.com>
2025-12-15 00:05:27 +0100

    feat: randomize start frame index in animated particle

--- src/scene/shader-lib/chunks/particle/vert/particleAnimFrameClamp.js
+++ src/scene/shader-lib/chunks/particle/vert/particleAnimFrameClamp.js
@@ -1,3 +1,3 @@
 export default /* glsl */`
-    float animFrame = min(floor(texCoordsAlphaLife.w * animTexParams.y) + animTexParams.x, animTexParams.z);
+    float animFrame = min(floor(texCoordsAlphaLife.w * animTexParams.y) + animTexParams.x + floor((animTexParams.z + 1.0) * rndFactor3.z), animTexParams.z);
 `;
